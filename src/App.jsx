779420fbf49e2b308937e241c8f3b32f480880a3
Jiseoup <jseoup@gmail.com>
2025-07-25 16:48:36 +0900

    Update loading provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Alert from './components/Common/Alert';
 import AxiosInterceptor from './components/Common/AxiosInterceptor';
+import Loading from './components/Common/Loading';
 import { AccessTokenProvider } from './contexts/AccessTokenContext';
 import { AlertProvider } from './contexts/AlertContext';
+import { LoadingProvider } from './contexts/LoadingContext';
 import Main from './pages/Main';
 import Register from './pages/Register';
 import theme from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <AlertProvider>
-        <AccessTokenProvider>
-          <AxiosInterceptor />
-          <Alert />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </BrowserRouter>
-        </AccessTokenProvider>
-      </AlertProvider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <AlertProvider>
+          <AccessTokenProvider>
+            <LoadingProvider>
+              <Loading />
+              <AxiosInterceptor />
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </LoadingProvider>
+          </AccessTokenProvider>
+        </AlertProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
