13d266ba704f47b36feea6067c4f1e20a59dead6
Jiseoup <jseoup@gmail.com>
2025-07-25 19:38:52 +0900

    Redesign login options

--- src/components/Login/LoginOptions/index.jsx
+++ src/components/Login/LoginOptions/index.jsx
@@ -1,6 +1,6 @@
-import { Checkbox, FormControlLabel } from '@mui/material';
+import { Checkbox } from '@mui/material';
 
-import { LoginOptionsBox, StyledLink } from './styled';
+import { LoginOptionsBox, StyledFormControlLabel, StyledLink } from './styled';
 
 function LoginOptions() {
   return (
@@ -8,10 +8,9 @@ function LoginOptions() {
       <LoginOptionsBox>
         {/* TODO: 로그인 상태 유지 구현 필요 */}
         {/* Stay Logged-In Checkbox Component. */}
-        <FormControlLabel
+        <StyledFormControlLabel
           label="로그인 상태 유지"
           control={<Checkbox size="small" />}
-          sx={{ ml: -1 }}
         />
 
         {/* TODO: 비밀번호 찾기 구현 필요 */}
--- src/components/Login/LoginOptions/styled.js
+++ src/components/Login/LoginOptions/styled.js
@@ -1,4 +1,4 @@
-import { Link } from '@mui/material';
+import { FormControlLabel, Link } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 import RowBox from '../../Common/RowBox';
@@ -8,13 +8,19 @@ export const LoginOptionsBox = styled(RowBox)({
   alignItems: 'center',
 });
 
+export const StyledFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
+  marginLeft: theme.spacing(-1),
+  '& .MuiFormControlLabel-label': {
+    color: theme.palette.link.main,
+  },
+}));
+
 export const StyledLink = styled(Link)(({ theme }) => ({
   fontSize: '14px',
   color: theme.palette.link.main,
   textDecorationColor: theme.palette.link.main,
   textDecorationThickness: '0.5px',
   textUnderlineOffset: '2px',
-
   '&:hover': {
     color: theme.palette.link.hover,
     textDecorationColor: theme.palette.link.hover,
