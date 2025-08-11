835ee503e4f7fb63610a0913c28e9d83c99ffc6c
Jiseoup <jseoup@gmail.com>
2025-08-11 20:53:23 +0900

    Update card component style

--- src/components/Common/Card/styled.js
+++ src/components/Common/Card/styled.js
@@ -1,12 +1,13 @@
+import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-export const Card = styled('div')({
-  background: '#ffffff',
-  boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.2)',
-  borderRadius: '20px',
-  padding: '56px 48px',
-  minWidth: '700px',
+export const Card = styled(Box)(({ theme }) => ({
   display: 'flex',
-  flexDirection: 'column',
   alignItems: 'center',
-});
+  flexDirection: 'column',
+  padding: theme.spacing(5),
+  background: '#FFFFFF',
+  boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.2)',
+  borderRadius: theme.spacing(2.5),
+  minWidth: '700px',
+}));
