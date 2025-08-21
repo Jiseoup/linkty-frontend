6e4acce11482a142df2bd4c15020f91a0fbf410a
Jiseoup <jseoup@gmail.com>
2025-08-22 01:41:36 +0900

    Refactor policy top card design

--- src/components/Common/Footer/PolicyTopCard/styled.js
+++ src/components/Common/Footer/PolicyTopCard/styled.js
@@ -2,14 +2,14 @@ import { Alert, Card, Chip, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const TopCard = styled(Card)(({ theme }) => ({
-  borderRadius: theme.spacing(2),
+  borderRadius: theme.spacing(1),
   padding: theme.spacing(4),
   marginBottom: theme.spacing(4), // TODO: 조정 필요할 수도 있음
   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
 }));
 
 export const EffectiveDateChip = styled(Chip)(({ theme }) => ({
-  padding: theme.spacing(2),
+  padding: theme.spacing(1),
   marginBottom: theme.spacing(2),
 }));
 
