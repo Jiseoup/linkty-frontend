8b0062f3d3c4243504002c460112afabd76628d6
Jiseoup <jseoup@gmail.com>
2025-10-30 19:19:51 +0900

    Apply ActionBar component to Urls Page.

--- src/pages/MyPage/Urls/index.jsx
+++ src/pages/MyPage/Urls/index.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 import LinkIcon from '@mui/icons-material/Link';
 
 import Content from '../../../components/MyPage/Content';
 import ContentBox from '../../../components/MyPage/ContentBox';
+import ActionBar from '../../../components/MyPage/Urls/ActionBar';
 
 function Urls() {
+  const [searchValue, setSearchValue] = useState(null);
+
+  const onSearchValueChange = (e) => setSearchValue(e.target.value);
+
   return (
     <ContentBox>
       <Content title="URL 관리" titleIcon={<LinkIcon sx={{ fontSize: 32 }} />}>
-        내용
+        {/* Header Action Bar Component. */}
+        <ActionBar
+          searchValue={searchValue}
+          onSearchValueChange={onSearchValueChange}
+        />
       </Content>
     </ContentBox>
   );
