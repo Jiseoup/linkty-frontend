5a587a08d0414b01a0461c6ae8eb58c239c4214c
Jiseoup <jseoup@gmail.com>
2025-11-28 19:54:07 +0900

    Refactor datetimepicker format

--- src/components/Common/DateTimePicker/index.jsx
+++ src/components/Common/DateTimePicker/index.jsx
@@ -14,7 +14,7 @@ function DateTimePicker(props) {
 }
 
 DateTimePicker.defaultProps = {
-  format: 'YYYY-MM-DD hh:mm',
+  format: 'YYYY-MM-DD HH:mm',
   slotProps: {
     textField: {
       fullWidth: true,
